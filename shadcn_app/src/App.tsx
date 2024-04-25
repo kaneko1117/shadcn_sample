8bab6d03eed03326ca36aed48cd18e2b52874373
kaneko1117 <yuta_kaneko@pj100.biz>
2024-04-25 17:43:02 +0900

    ✨form

--- shadcn_app/src/App.tsx
+++ shadcn_app/src/App.tsx
@@ -1,14 +1,44 @@
+import { useForm } from "react-hook-form";
+
 import { Button } from "./components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "./components/ui/form";
+import { Input } from "./components/ui/input";
 
 function App() {
+  const form = useForm<{ name: string }>({
+    mode: "onBlur",
+    defaultValues: { name: "aiful" },
+  });
+  const onSubmit = (data: { name: string }) => {
+    console.log(data);
+  };
   return (
-    <div className="flex space-x-4">
-      <Button variant="ghost">TEXT</Button>
-      <Button className="rounded-[30px]">CONTAINED</Button>
-      <Button variant="outline" className="hover:bg-sky-200">
-        OUTLINE
-      </Button>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="name"
+          rules={{ required: "入力してください" }}
+          render={({ field, fieldState }) => (
+            <FormItem>
+              <FormControl>
+                <Input {...field} className="w-[200px] my-[10px]" />
+              </FormControl>
+              <FormMessage>{fieldState.error?.message}</FormMessage>
+            </FormItem>
+          )}
+        />
+        <Button variant="outline" type="submit" className="hover:bg-sky-200">
+          OUTLINE
+        </Button>
+      </form>
+    </Form>
   );
 }
 
